58ceaa8dde1efeba631bb71b96c67efb3f8dcba6
Martin Brandl <mjibrandl@gmail.com>
2020-08-07 11:42:06 +0200

    added startup

---
+++ Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using azure_functions_cosmosclient;
+using Microsoft.Azure.Cosmos.Fluent;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace azure_functions_cosmosclient
+{
+    public class Startup : FunctionsStartup
+    {
+        private static readonly IConfigurationRoot Configuration = new ConfigurationBuilder()
+            .SetBasePath(Environment.CurrentDirectory)
+            .AddJsonFile("appsettings.json", true)
+            .AddEnvironmentVariables()
+            .Build();
+
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+           builder.Services.AddSingleton(s => {
+                var connectionString = Configuration["CosmosDBConnection"];
+                if (string.IsNullOrEmpty(connectionString))
+                {
+                    throw new InvalidOperationException(
+                        "Please specify a valid CosmosDBConnection in the appSettings.json file or your Azure Functions Settings.");
+                }
+
+                return new CosmosClientBuilder(connectionString)
+                    .Build();
+           });
+        }
+    }
+}
